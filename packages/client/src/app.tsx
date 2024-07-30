fc3ec63c1bed93c646eb06ac1076c7cc2867d51f
x0finch <x0finch0x@gmail.com>
2024-07-31 00:35:29 +0800

    feat(client): use dark theme as default

--- packages/client/src/app.tsx
+++ packages/client/src/app.tsx
@@ -1,11 +1,15 @@
+import { ThemeProvider } from '@shadcn/components/theme-provider'
 import { Button } from '@shadcn/components/ui/button'
 
 export function App() {
   return (
-    <div className='flex justify-center items-center'>
-      <Button>aaa</Button>
-    </div>
+    <ThemeProvider defaultTheme='dark' storageKey='shadcn-ui-theme'>
+      <div className='flex justify-center items-center'>
+        <Button>aaa</Button>
+      </div>
+    </ThemeProvider>
   )
+
 }
 
 export default App
