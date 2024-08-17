ab2ae221f8b2f4c77d587c715246e5f2238bacd7
x0finch <x0finch0x@gmail.com>
2024-08-18 02:12:27 +0800

    feat(client): add footer with poweredBy and github link

--- packages/client/src/routes/root.tsx
+++ packages/client/src/routes/root.tsx
@@ -1,6 +1,7 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import { Navbar } from "~/components/nav-bar";
 import { Analytics } from "@vercel/analytics/react";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
 
 const Root = () => {
   return (
@@ -9,8 +10,40 @@ const Root = () => {
       <main className="flex-1">
         <Outlet />
       </main>
+      <Footer />
       <Analytics />
     </div>
+  );
+};
+
+const GithubLink = () => {
+  return (
+    <Link
+      target="_blank"
+      rel="noopener noreferrer"
+      to="https://github.com/x0finch/0xlimit"
+    >
+      <GitHubLogoIcon className="h-4 w-4" />
+    </Link>
+  );
+};
+
+const PoweredBy = () => {
+  return (
+    <div className="text-xs text-muted-foreground">
+      Powered by Uniswap Liquidity V3
+    </div>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer className="flex justify-center items-center relative px-4 py-1">
+      <PoweredBy />
+      <div className="absolute right-2">
+        <GithubLink />
+      </div>
+    </footer>
   );
 };
 
