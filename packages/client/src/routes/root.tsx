9e1afa09537a5d107a1c2477b0fc427c8ad302ea
x0finch <x0finch0x@gmail.com>
2024-08-18 18:47:21 +0800

    refactor(client): update footer

--- packages/client/src/routes/root.tsx
+++ packages/client/src/routes/root.tsx
@@ -1,7 +1,6 @@
 import { Link, Outlet } from "react-router-dom";
 import { Navbar } from "~/components/nav-bar";
 import { Analytics } from "@vercel/analytics/react";
-import { GitHubLogoIcon } from "@radix-ui/react-icons";
 
 const Root = () => {
   return (
@@ -16,32 +15,20 @@ const Root = () => {
   );
 };
 
-const GithubLink = () => {
-  return (
-    <Link
-      target="_blank"
-      rel="noopener noreferrer"
-      to="https://github.com/x0finch/0xlimit"
-    >
-      <GitHubLogoIcon className="h-4 w-4" />
-    </Link>
-  );
-};
-
-const PoweredBy = () => {
-  return (
-    <div className="text-xs text-muted-foreground">
-      Powered by Uniswap Liquidity V3
-    </div>
-  );
-};
-
 const Footer = () => {
   return (
-    <footer className="flex justify-center items-center relative px-4 py-1">
-      <PoweredBy />
-      <div className="absolute right-2">
-        <GithubLink />
+    <footer className="flex items-center relative px-4 py-1">
+      <div className="text-[11px] text-muted-foreground">
+        Powered by Uniswap Liquidity V3, the source code is available on{" "}
+        <Link
+          to="https://github.com/x0finch/0xlimit"
+          className="underline"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </Link>
+        .
       </div>
     </footer>
   );
